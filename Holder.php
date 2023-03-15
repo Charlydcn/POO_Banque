5fee97898fdac3c11755a3d33275d444235630e1
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-15 16:00:19 +0100

    "Date of birth" of Holder's __toString to "Age"

--- Holder.php
+++ Holder.php
@@ -95,7 +95,7 @@ class Holder
     {
         return "<strong>Last name : </strong>" . $this->_lastName . "<br>"
         . "<strong>First name : </strong>" . $this->_firstName . "<br>"
-        . "<strong>Date of birth : </strong>". $this->getAge() . "<br>" // J'utilise ici le getBirthDate et non directement le _birthDate pour assurer la conversion en string du DateTime
+        . "<strong>Age : </strong>". $this->getAge() . "<br>" // J'utilise ici le getBirthDate et non directement le _birthDate pour assurer la conversion en string du DateTime
         . "<strong>City : </strong>". $this->_city . "<br>"
         . "<strong>Bank Accounts : </strong>" . $this->getBankAccounts(); // J'utilise ici le getBankAccounts et non directement le _bankAccounts pour ne pas convertir un tableau en string
     }
